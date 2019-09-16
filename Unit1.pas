b40ad532de100d850e3fbf94eae1bec923a10fc1
yamat0jp <terukohietori@gmail.com>
2019-09-16 20:29:06 +0900

    進展

--- Unit1.pas
+++ Unit1.pas
@@ -31,6 +31,17 @@ function StartMouseIMEHook(hWnd: hWnd): Boolean; stdcall;
   external 'keyhook.dll';
 procedure StopMouseIMEHook; stdcall; external 'keyhook.dll';
 
+function GetPoint: TPoint;
+var
+  s1, s2: Cardinal;
+begin
+  s1 := GetCurrentThreadID;
+  s2 := GetWindowThreadProcessID(GetForegroundWindow);
+  AttachThreadInput(s1, s2, true);
+  GetCaretPos(result);
+  AttachThreadInput(s1, s2, false);
+end;
+
 procedure TForm1.FormCreate(Sender: TObject);
 begin
   ChangeWindowMessageFilter(WM_APP, MSGFLT_ADD);
@@ -47,15 +58,12 @@ end;
 
 procedure TForm1.Timer1Timer(Sender: TObject);
 var
-  s1, s2: Cardinal;
   p: TPoint;
   i: Integer;
 begin
-  s1 := GetCurrentThreadID;
-  s2 := GetWindowThreadProcessID(GetForegroundWindow);
-  AttachThreadInput(s1, s2, true);
-  GetCaretPos(p);
-  AttachThreadInput(s1, s2, false);
+  if Visible = false then
+    Exit;
+  p := GetPoint;
   for i := 1 to 3 do
   begin
     if Left > p.X then
@@ -72,13 +80,19 @@ end;
 procedure TForm1.WMAPP(var Msg: TMessage);
 var
   pEvent: PEventMSG;
+  p: TPoint;
 begin
   pEvent := PEventMSG(Msg.LParam);
   case pEvent.message of
     WM_MOUSEMOVE:
       Hide;
     WM_KEYDOWN:
-      Show;
+      begin
+        p := GetPoint;
+        Left := p.X - ClientWidth div 4;
+        Top := p.Y - ClientHeight div 2;
+        Show;
+      end;
   end;
 end;
 
